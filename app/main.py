6cceb95f1974bcfd53d1dcf152426c9d170083fc
THI TRIEU XUYEN NGUYEN <u1252970@umail.utah.edu>
2024-12-05 15:51:48 -0700

    handled cors policy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.api import mp_hand
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()  # Create a FastAPI app instance
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(mp_hand.router, prefix="/hands", tags=["hands"])
 
